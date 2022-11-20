58244889d91604538276a6df89559e82ecbc96ed
Simen A. W. Olsen <so@bjerk.io>
2022-11-20 21:58:17 +0100

    add developers and procore-abax project

--- resources/domain/procore-abax.ts
+++ resources/domain/procore-abax.ts
@@ -1,10 +1,10 @@
 import { Config } from '@pulumi/pulumi';
 import { GithubGCPProject } from '../components/github-gcp-project';
 
-const config = new Config('todoist-github-bot');
+const config = new Config('procore-abax');
 
-new GithubGCPProject('todoist-github-bot', {
-  repo: 'todoist-github-bot',
-  projectId: 'todoist-github-bot',
+new GithubGCPProject('procore-abax', {
+  repo: 'procore-abax',
+  projectId: 'procore-abax',
   developers: config.requireObject<string[]>('developers'),
 });
