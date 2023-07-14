10df31c4a6b0683bb801ec522244a01728ff3dd6
Simen A. W. Olsen <so@bjerk.io>
2023-07-14 17:07:41 +0200

    fix config ref

--- resources/kubernetes/todoist-github/event-handler/config.ts
+++ resources/kubernetes/todoist-github/event-handler/config.ts
@@ -1,6 +1,6 @@
 import * as pulumi from '@pulumi/pulumi';
 
-const config = new pulumi.Config('todoist-github-bot-event-handler');
+const config = new pulumi.Config('todoist-github-event-handler');
 export const host = config.requireSecret('host');
 export const tag = config.require('tag');
 
