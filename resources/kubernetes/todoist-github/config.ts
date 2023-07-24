7b346bc097d6e0c6b9685660b39a6a700040849f
Simen A. W. Olsen <so@bjerk.io>
2023-07-24 23:57:54 +0200

    refactor: use server for todoist-github

--- resources/kubernetes/todoist-github/config.ts
+++ resources/kubernetes/todoist-github/config.ts
@@ -1,4 +1,5 @@
 import * as pulumi from '@pulumi/pulumi';
 
 const config = new pulumi.Config('todoist-github');
+export const host = config.require('host');
 export const tag = config.require('tag');
