d4452527efbc32ae8546801ca42642b0ad092696
Simen A. W. Olsen <so@bjerk.io>
2023-08-03 09:36:57 +0200

    fix reference to unleash

--- resources/kubernetes/unleash/config.ts
+++ resources/kubernetes/unleash/config.ts
@@ -3,4 +3,4 @@ import * as pulumi from '@pulumi/pulumi';
 const config = new pulumi.Config('unleash');
 
 export const host = config.require('host');
-export const image = config.require('image');
+export const image = config.require('ext-image');
