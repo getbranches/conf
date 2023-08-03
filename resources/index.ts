8b4e8748269992813a35f5abca3005012093febf
Simen A. W. Olsen <so@bjerk.io>
2023-08-03 12:11:30 +0200

    add meti import and sort imports

--- resources/index.ts
+++ resources/index.ts
@@ -1,26 +1,27 @@
 import './config';
 import './google/artifact-registry';
-import './google/project';
 import './google/gke';
 import './google/iam';
+import './google/postgres-backup';
+import './google/project';
 import './google/slack-logger';
+import './repository-with-artifacts';
+
+// Kubernetes charts and other resources
 import './kubernetes/ingress-controller.chart';
 import './kubernetes/postgres-operator.chart';
 import './kubernetes/provider';
-import './repository-with-artifacts';
 
-import './google/postgres-backup';
-
-// Kubernetes resources
-// import './kubernetes/procore-abax/deployment';
-// import './kubernetes/procore-abax/ingress';
-import './kubernetes/deployments/procore-abax';
+// Kubernetes Deployments
 import './kubernetes/deployments/abax-minuba';
-import './kubernetes/tripletex-project-reporter/deployment';
-import './kubernetes/tripletex-project-reporter/ingress';
-import './kubernetes/vaultwarden/deployment';
-import './kubernetes/vaultwarden/ingress';
-import './kubernetes/unleash/deployment';
-import './kubernetes/unleash/ingress';
+import './kubernetes/deployments/meti';
+import './kubernetes/deployments/procore-abax';
 import './kubernetes/todoist-github/deployment';
 import './kubernetes/todoist-github/ingress';
+import './kubernetes/tripletex-project-reporter/deployment';
+import './kubernetes/tripletex-project-reporter/ingress';
+import './kubernetes/unleash/deployment';
+import './kubernetes/unleash/ingress';
+import './kubernetes/vaultwarden/deployment';
+import './kubernetes/vaultwarden/ingress';
+
