9928103c5c1c5c6654558be6f3715b4fcf3fedb8
Xillians <sandra.toftemo@bjerk.io>
2023-08-08 13:08:47 +0200

    create ingress

---
+++ resources/kubernetes/deployments/abax-minuba-db/ingress.ts
@@ -0,0 +1,41 @@
+import * as k8s from '@pulumi/kubernetes';
+import { provider } from '../../provider';
+import { host } from './config';
+import { port, service } from './deployment';
+
+const name = 'abax-minuba-db';
+
+export const ingress = new k8s.networking.v1.Ingress(
+  `${name}-ingress`,
+  {
+    metadata: {
+      name,
+      annotations: {
+        'kubernetes.io/ingress.class': 'caddy',
+        'pulumi.com/skipAwait': 'true',
+      },
+    },
+    spec: {
+      rules: [
+        {
+          host,
+          http: {
+            paths: [
+              {
+                path: '/',
+                pathType: 'Prefix',
+                backend: {
+                  service: {
+                    name: service.metadata.name,
+                    port: { number: port },
+                  },
+                },
+              },
+            ],
+          },
+        },
+      ],
+    },
+  },
+  { provider, deleteBeforeReplace: true },
+);
