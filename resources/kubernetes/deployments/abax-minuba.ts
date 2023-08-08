744eb48a23eac1efdecc14327f8f77d8fd60d9e0
Simen A. W. Olsen <so@bjerk.io>
2023-08-08 11:04:14 +0200

    feat: add server for minuba (#158)
    Co-authored-by: Brage Sekse Aarset <brage@bjerk.io>

--- resources/kubernetes/deployments/abax-minuba.ts
+++ resources/kubernetes/deployments/abax-minuba.ts
@@ -9,9 +9,37 @@ export const standardDeployment = new StandardDeployment(
   {
     image: config.require('ui-image'),
     tag: config.require('tag'),
-    host: config.require('host'),
+    host: config.require('frontend-host'),
     logLevel: 'debug',
     healthCheckHttpPath: '/api/health',
+  },
+  { providers: [provider] },
+);
+
+export const serverDeployment = new StandardDeployment(
+  'abax-minuba-server',
+  {
+    image: config.require('server-image'),
+    tag: config.require('tag'),
+    host: config.require('api-host'),
+    logLevel: 'debug',
+    secretEnv: {
+      FRONTEND_URL: pulumi
+        .output(config.require('frontend-host'))
+        .apply(host => `https://${host}`),
+        ABAX_CLIENT_ID: config.require('abax-client-id'),
+        ABAX_CLIENT_SECRET: config.require('abax-client-secret'),
+    },
+    ports: [
+      {
+        port: 8080,
+        name: 'public',
+      },
+      {
+        port: 8081,
+        name: 'management',
+      },
+    ],
   },
   { providers: [provider] },
 );
