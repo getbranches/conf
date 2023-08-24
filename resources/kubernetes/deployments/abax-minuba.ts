69fd9aebc68f40e80af18a862a38313361d46dcc
Sandra Viktoria D. Toftemo <sandra.toftemo@gmail.com>
2023-08-24 15:29:01 +0200

    add "secret" env vars (#230)

--- resources/kubernetes/deployments/abax-minuba.ts
+++ resources/kubernetes/deployments/abax-minuba.ts
@@ -31,6 +31,8 @@ export const standardDeployment = new StandardDeployment(
     secretEnv: {
       ABAX_CLIENT_ID: config.require('abax-client-id'),
       ABAX_CLIENT_SECRET: config.require('abax-client-secret'),
+      NEXT_PUBLIC_HELLO: 'hello',
+      HELLO: 'hello',
     },
     logLevel: 'debug',
     healthCheckHttpPath: '/',
