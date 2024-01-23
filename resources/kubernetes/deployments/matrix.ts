f59a1f1bf0e92281098a38a646e2e2db7c0ab7d4
Brage Sekse Aarset <brage@bjerk.io>
2024-01-23 13:22:57 +0200

    remove data from path

--- resources/kubernetes/deployments/matrix.ts
+++ resources/kubernetes/deployments/matrix.ts
@@ -98,8 +98,8 @@ export const synapseDeployment = new StandardDeployment(
     // this command is needed to load the secrets and config in two separate files
     command: [
       'run',
-      '--config-path=/data/homeserver.yaml',
-      '--config-path=/data/secrets.yaml',
+      '--config-path=/homeserver.yaml',
+      '--config-path=/secrets.yaml',
     ],
   },
   { providers: [provider] },
