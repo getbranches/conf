31c484d6b28709cde29c40bb9f09d01bd644f95e
Brage Sekse Aarset <brage@bjerk.io>
2024-02-05 08:42:21 +0200

    fix: remove comment

--- resources/kubernetes/matrix/fuse-csi.ts
+++ resources/kubernetes/matrix/fuse-csi.ts
@@ -10,11 +10,6 @@ new gcp.serviceaccount.IAMBinding(
     serviceAccountId: matrixSynapseServiceAccount.name,
     role: 'roles/iam.serviceAccountTokenCreator',
     members: [
-      // Construct the Kubernetes service account member string
-      // It should be in the format "serviceAccount:PROJECT_ID.svc.id.goog[K8S_NAMESPACE/K8S_SA_NAME]"
-      // PROJECT_ID should be replaced by your Google Cloud project ID
-      // K8S_NAMESPACE should be replaced by your Kubernetes service account namespace
-      // K8S_SA_NAME should be replaced by your Kubernetes service account name
       pulumi.interpolate`serviceAccount:${project.projectId}.svc.id.goog[${matrixK8sServiceAccount.metadata.namespace}/${matrixK8sServiceAccount.metadata.name}]`,
     ],
   },
