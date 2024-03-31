f0d06403c5665353b5abf111d55dce8eba1b84cb
Simen A. W. Olsen <so@bjerk.io>
2024-03-31 22:01:27 +0200

    clean out old files

--- resources/kubernetes/vaultwarden/config.ts
+++
@@ -1,9 +0,0 @@
-import * as pulumi from '@pulumi/pulumi';
-
-const config = new pulumi.Config('vaultwarden');
-
-export const host = config.require('host');
-export const image = config.require('ext-image');
-export const adminToken = config.requireSecret('admin-token');
-export const yubicoClientId = config.requireSecret('yubico-client-id');
-export const yubicoClientSecret = config.requireSecret('yubico-client-secret');
