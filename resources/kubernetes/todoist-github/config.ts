7956ccfe912b9892c172a797766982a5d8c0c9a9
Simen A. W. Olsen <hello@simenandre.no>
2024-11-14 14:30:30 +0100

    quick cleanup

--- resources/kubernetes/todoist-github/config.ts
+++
@@ -1,7 +0,0 @@
-import * as pulumi from '@pulumi/pulumi';
-
-const config = new pulumi.Config('todoist-github');
-export const host = config.require('host');
-export const tag = config.require('tag');
-export const webhookSecret = config.requireSecret('webhook-secret');
-export const logLevel = config.get('log-level') ?? 'info';
--- resources/utils.ts
+++
@@ -1,4 +0,0 @@
-import * as gcp from '@pulumi/gcp';
-export const nullProvider = new gcp.Provider('google-null-provider', {
-  project: 'not-really-used',
-});
